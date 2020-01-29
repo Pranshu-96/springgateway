4a77271003776c25ba7eec327cc190c862a7922e
Pranshu-96 <56105649+Pranshu-96@users.noreply.github.com>
2020-01-29 13:59:35 +0530

    Add files via upload

---
+++ spring-boot-zuulgatewayproxy-student-service/src/test/java/com/example/springbootzuulgatewayproxystudentservice/SpringBootZuulgatewayproxyStudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootzuulgatewayproxystudentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZuulgatewayproxyStudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
